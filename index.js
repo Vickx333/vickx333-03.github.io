9793568a5bda6d583df335da41bc186c55368f37
Vickx333 <98500794+Vickx333@users.noreply.github.com>
2022-11-24 10:04:00 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var fondo =  document.getElementById('fondo');
+
+function mover2(){
+    var fondo =  document.getElementById('fondo');
+    fondo.style.marginLeft = '163px';
+
+}
+function origen(){
+    var fondo =  document.getElementById('fondo');
+
+    fondo.style.marginLeft = '0';   
+}
+
+function mover3(){
+    var fondo =  document.getElementById('fondo');
+    fondo.style.marginLeft = '329px';
+
+}
+function mover4(){
+    var fondo =  document.getElementById('fondo');
+    fondo.style.marginLeft = '495px';
+
+}
+function mover5(){
+    var fondo =  document.getElementById('fondo');
+    fondo.style.marginLeft = '658px';
+
+}
